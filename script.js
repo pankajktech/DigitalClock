873fdcbeb8a253499e7005cea2f987dc8669c484
PankajKTech <pankajkatech@gmail.com>
2022-10-08 23:59:11 +0530

    updated

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ function updateTime() {
     let seconds = dateTime.getSeconds()
 
     let am_or_pm = document.getElementById("am_or_pm")
-    if(hours >= 12){
-        hours = hours-12
-    }
-
     if (hours >= 12) {
         am_or_pm.innerHTML = "PM"
 
     } else {
         am_or_pm.innerHTML = "AM"
     }
+    if(hours >= 12){
+        hours = hours-12
+    }
+
    
     document.getElementById("hours").innerHTML = hours
     document.getElementById("minutes").innerHTML = minutes
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'clock' : ['Orbitron']
+      }
+    },
+    
   },
   plugins: [],
 }
